5be24045055f3cbd22b93ede51638e87a91cd1ee
BenJilks <benjyjilks@gmail.com>
2024-09-21 18:29:48 +0100

    Specify scripts to evaluate via the command lines.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,6 @@
-mod ast;
-mod error;
-mod evaluate;
-mod frame;
-mod scope;
-
 use ast::set_program_file_path;
 use evaluate::evaluate_program;
-use std::error::Error;
+use std::env::args;
 use std::fs::File;
 use std::io::{stdout, Read};
 use std::path::PathBuf;
@@ -15,8 +9,14 @@ use std::process::ExitCode;
 use lalrpop_util::lalrpop_mod;
 lalrpop_mod!(pub macro_parser);
 
-fn main() -> Result<ExitCode, Box<dyn Error>> {
-    let mut file = File::open("test.bfm")?;
+mod ast;
+mod error;
+mod evaluate;
+mod frame;
+mod scope;
+
+fn evaluate_file(file_path: &str) -> std::io::Result<bool> {
+    let mut file = File::open(file_path)?;
     let mut script = String::new();
     file.read_to_string(&mut script)?;
 
@@ -24,7 +24,24 @@ fn main() -> Result<ExitCode, Box<dyn Error>> {
     let mut program = parser.parse(&script).unwrap();
     set_program_file_path(&mut program, &PathBuf::from("test.bfm"));
 
-    if evaluate_program(&mut stdout(), &program)? {
+    evaluate_program(&mut stdout(), &program)
+}
+
+fn main() -> std::io::Result<ExitCode> {
+    let mut args = args();
+
+    let executable = args.next().unwrap();
+    if args.len() == 0 {
+        eprintln!("{}: error: no input files given", executable);
+        return Ok(ExitCode::FAILURE);
+    }
+
+    let mut did_error = false;
+    for file_path in args {
+        did_error |= evaluate_file(&file_path)?;
+    }
+
+    if did_error {
         Ok(ExitCode::FAILURE)
     } else {
         Ok(ExitCode::SUCCESS)
